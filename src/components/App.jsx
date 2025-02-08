cf5a37a9a545d260c933faaca8eed359bd7dcf5a
María García Antúnez <maria.garcia.antunez@gmail.com>
2025-02-08 10:26:04 +0100

    Add styles to form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import '../styles/App.scss';
 import '../styles/_header.scss';
 import '../styles/_movies_list.scss';
+import '../styles/_form.scss';
 import { useState } from 'react';
 
 function App() {
@@ -12,28 +13,28 @@ function App() {
         <h1 className="header__title">The Owen Wilson WOW web</h1>
       </header>
 
-      <form>
-
-        <input
-          className="header__search"
-          autoComplete="off"
-          type="search"
-          name="search"
-          placeholder="Filtrar por película"
-          value=""
-        />
-
-        <select className="header__search" name="searchYear">
-          <option value="">Filtrar por año</option>
-          {Array.from({ length: 2025 - 1990 + 1 }, (_, i) => {
-            const year = 1990 + i;
-            return <option key={year} value={year}>{year}</option>;
-          })}
-        </select>
-
-      </form>
-
       <main>
+
+        <form>
+
+          <input
+            className="form__search"
+            autoComplete="off"
+            type="search"
+            name="search"
+            placeholder="Filtrar por película"
+            value=""
+          />
+
+          <select className="form__search" name="searchYear">
+            <option value="">Filtrar por año</option>
+            {Array.from({ length: 2025 - 1990 + 1 }, (_, i) => {
+              const year = 1990 + i;
+              return <option key={year} value={year}>{year}</option>;
+            })}
+          </select>
+
+        </form>
 
         <ul className="movies__list">
 
