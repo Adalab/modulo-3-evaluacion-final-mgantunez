e623b73aba4e87fd2a46d34d94dc04831afe7b17
María García Antúnez <maria.garcia.antunez@gmail.com>
2025-02-08 09:41:04 +0100

    Add ul and inputs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,66 @@ import '../styles/App.scss';
 function App() {
   return (
     <div className="page">
-      <header className="header"></header>
-      <h1 className="header__title">The Owen Wilson WOW web</h1>
+
+      <header className="header">
+        <h1 className="header__title">The Owen Wilson WOW web</h1>
+      </header>
+
+      <form>
+
+        <input
+          className="header__search"
+          autoComplete="off"
+          type="search"
+          name="search"
+          placeholder="Filtrar por película"
+          value=""
+        />
+
+        <select className="header__search" name="searchYear">
+          <option value="">Filtrar por año</option>
+          {Array.from({ length: 2025 - 1990 + 1 }, (_, i) => {
+            const year = 1990 + i;
+            return <option key={year} value={year}>{year}</option>;
+          })}
+        </select>
+
+      </form>
+
+      <main>
+
+        <ul className="movies__list">
+
+          <li className="movies__item">
+
+            <img src="#" alt="poster" />
+            <p>Película</p>
+            <p>Frase completa</p>
+            <p>Año</p>
+
+          </li>
+
+          <li className="movies__item">
+
+            <img src="#" alt="poster" />
+            <p>Película</p>
+            <p>Frase completa</p>
+            <p>Año</p>
+
+          </li>
+
+          <li className="movies__item">
+
+            <img src="#" alt="poster" />
+            <p>Película</p>
+            <p>Frase completa</p>
+            <p>Año</p>
+
+          </li>
+
+        </ul>
+
+      </main>
     </div>
   )
 }
