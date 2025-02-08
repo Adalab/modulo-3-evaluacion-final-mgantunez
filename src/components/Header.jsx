00cfcba8a283c76af3755eac8cd87665fbe7d908
María García Antúnez <maria.garcia.antunez@gmail.com>
2025-02-08 13:53:05 +0100

    Fix header and add subtitle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ function Header() {
         <header className="header">
             <img className="header__image" src="/portada_header.png" alt="Owen Wilson WOW" />
             <h1 className="header__title">The Owen Wilson WOW web</h1>
+            <h2 className="header__subtitle">Busca una película con el nombre exacto en inglés y déjate sorprender por todas las veces que Owen Wilson dice WOW</h2>
         </header>
     );
 
