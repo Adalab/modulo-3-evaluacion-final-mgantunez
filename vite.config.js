4e77f24551bf63c5db026653330c67a994e1d388
María García Antúnez <maria.garcia.antunez@gmail.com>
2025-02-10 20:13:29 +0100

    Update GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/modulo-3-evaluacion-final-mgantunez/',
   plugins: [react()],
   server: {
     open: "/index.html",
